13aaee297cec7f2ebf6ee4a4b10124b4fcb0c496
icywaffle <synkre@gmail.com>
2019-03-22 21:58:39 -0700

    Cleaner Design.Doc
    The SQL database design has been greatly simplified. We only care about the database holding the price history that we're going to crawl. The item information can be saved to a cache layer.

--- items/searchxivapi.go
+++ items/searchxivapi.go
@@ -6,7 +6,9 @@ import (
 	getKeys "./keys"
 )
 
-// TODO: ?columns=Attributes,Object.Attribute will significantly lower payload.
+// Appends the strings to the xivapi.com
+
+// TODO: ?columns=Attributes,Object.Attribute will significantly lower payload
 
 func UrlAPI(userfield string, useruniqueID string) string {
 	basewebsite := []byte("https://xivapi.com/")
@@ -28,10 +30,3 @@ func UrlAPI(userfield string, useruniqueID string) string {
 
 	return s
 }
-
-// Now that we can append, we need to choose options.
-// We need the user to be able to search for an item.
-//It's tedious to write
-// AmountIngredient0-9,
-// Name,ItemResultTargetID,ID,Url,
-// and this 9 times -> ItemIngredientRecipe0.*.CraftType,ItemIngredientRecipe0.*.ItemResult.ID,ItemIngredientRecipe0.*.ItemResult.Name
