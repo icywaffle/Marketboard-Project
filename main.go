a7964e3b0b7290219265d3e0fa388ff11f00b296
icywaffle <synkre@gmail.com>
2019-03-29 11:23:16 -0700

    Changed the Main.Go interface
    New for loop to handle the choice cases, in case we need to continously search for an item, or if we finally have what we want to use, we choose it.

--- main.go
+++ main.go
@@ -3,37 +3,55 @@ package main
 import (
 	"bufio"
 	"fmt"
-	"log"
 	"os"
-	"time"
 
 	xivapi "./xivapi"
 )
 
-func main() {
-
-	start := time.Now()
-
+func search() {
 	fmt.Printf("XIVAPI Search:")
 	scanner := bufio.NewScanner(os.Stdin)
 	scanner.Scan()
 	usersearchinput := scanner.Text()
 	//Searches the item, and put the json into an array.
 	xivapi.SearchItem(xivapi.UrlSearch(usersearchinput))
-
+}
+func choose() {
 	//After finding the results, and user chooses put the item ID into here.
 	fmt.Printf("ItemID:")
+	scanner := bufio.NewScanner(os.Stdin)
 	scanner.Scan()
 	useriteminput := scanner.Text()
 	fmt.Printf("Item/Recipe:")
 	scanner.Scan()
 	userchoiceinput := scanner.Text()
-	//We need to get the URL
 	testitem := xivapi.UrlRecipe(userchoiceinput, useriteminput)
+
 	//We need to use the GET request, and put JSON data into the database.
 	xivapi.GetRecipe(testitem)
+}
 
-	elapsed := time.Since(start)
-	log.Printf("Runtime: %s", elapsed)
+func main() {
+	//We need to get the URL
+	for {
+		var input int
+		n, err := fmt.Scanln(&input)
+		// Force choose a positive number
+		if n < 1 || err != nil {
+			fmt.Println("invalid input")
+			return
+		}
+		// If you need to re-search, use case 1.
+		// If you have the right itemID and Recipe, move onto case 2.
+		switch input {
+		case 1:
+			search()
+		case 2:
+			choose()
+		default:
+			os.Exit(2000) // ERROR 2000 : End program with no input.
+		}
+	}
+
 	// Current Status: Able to search, run, and obtain the Amount of ingredients, and ingredients of items
 }
