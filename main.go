c16c887b7e1271876fc51664cde6368ecbf99f80
icywaffle <synkre@gmail.com>
2019-05-16 21:36:09 -0700

    ISSUES: Changes in jsonconv.
    Attempts to separate the matrecipeIDslice, to be able to use this information to give prices of all the associated items. This doesn't work since this information doesn't exist on the page.

--- main.go
+++ main.go
@@ -1,58 +1,42 @@
 package main
 
 import (
-	"fmt"
 	"strconv"
 
 	xivapi "./xivapi"
-	database "./xivapi/database"
 )
 
-// Uses the Web Appending Function to create the url to request.
-func search() {
-	// Prompt for Search Input
-	fmt.Printf("XIVAPI Search:")
-	fmt.Printf("ID:")
-	var usersearchinput string
-	fmt.Scan(&usersearchinput)
-
-	// Pass the input to the Web Appending Function
-	urlxivapisearch := xivapi.UrlSearch(usersearchinput)
-
-	// Then use the xivapi to search for results
-	xivapi.SearchItem(urlxivapisearch)
-}
-
 func main() {
-	// Allows user to select what they want to do.
-	for {
-		var input int
-		var userchoice string
-		var userID int
-		fmt.Printf("Input Integer: Search(1), Find Recipe(2), Find Item(3), Get  Item Prices(4):")
-		fmt.Scanln(&input)
+	xivapi.GetItemPrices(xivapi.UrlPrices(strconv.Itoa(33180)), 33180)
+	/*
+		// Allows user to select what they want to do.
+		for {
+			var input int
+			var userchoice string
+			var userID int
+			fmt.Printf("Input Integer: ")
+			fmt.Scanln(&input)
 
-		switch input {
-		case 1:
-			search()
-		case 2:
-			fmt.Printf("Recipe ID:")
-			fmt.Scan(&userID)
-			userchoice = "recipe"
-			//Gets the item recipe, and puts it into the database.
-			xivapi.Getitem(xivapi.UrlItemRecipe(userchoice, strconv.Itoa(userID)), userchoice)
-		case 3:
-			fmt.Printf("Item ID:")
-			fmt.Scan(&userID)
-			userchoice = "item"
-			xivapi.Getitem(xivapi.UrlItemRecipe(userchoice, strconv.Itoa(userID)), userchoice)
-		case 4:
-			database.MongoFind("recipeid", 33180)
-		case 5:
-			xivapi.GetItemPrices(xivapi.UrlPrices(strconv.Itoa(14146)), 14146)
-		default:
-			fmt.Println("Invalid Case Selected.")
-			continue
-		}
-	}
+			switch input {
+			case 1:
+				fmt.Printf("Recipe ID:")
+				fmt.Scan(&userID)
+				userchoice = "recipe"
+				//Gets the item recipe, and puts it into the database.
+				xivapi.Getitem(xivapi.UrlItemRecipe(userchoice, strconv.Itoa(userID)), userchoice)
+			case 2:
+				fmt.Printf("Item ID:")
+				fmt.Scan(&userID)
+				userchoice = "item"
+				xivapi.Getitem(xivapi.UrlItemRecipe(userchoice, strconv.Itoa(userID)), userchoice)
+			case 3:
+				database.MongoFind("recipeid", 33180)
+			case 4:
+				xivapi.GetItemPrices(xivapi.UrlPrices(strconv.Itoa(33180)), 33180)
+			default:
+				fmt.Println("Invalid Case Selected.")
+				continue
+			}
+		}*/
+
 }
